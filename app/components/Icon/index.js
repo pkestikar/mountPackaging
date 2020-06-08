9b5cc89ab97d898f583742022d82c9d267b39a7d
Prathmesh Kestikar <pkestikar@gmail.com>
2020-06-08 23:36:19 +0100

    updates for the icon component

--- app/components/Icon/index.js
+++ app/components/Icon/index.js
@@ -1,1 +1,23 @@
 import React from 'react';
+import { ReactSVG } from "react-svg";
+
+const Icon = (props) => {
+    return (
+        <ReactSVG
+            src={`${props.iconPath}`}
+            beforeInjection={(svg) => {
+                svg.setAttribute('style', `height: ${props.sizeInPixels}px; width: ${props.sizeInPixels}px`);
+            }}
+            wrapper={'span'}
+            className={'mp--icon'}
+            onClick={() => props.onClick}
+        />
+    );
+};
+
+Icon.defaultProps = {
+    onClick: () => {},
+    sizeInPixels: '16'
+};
+
+export default Icon;
--- app/components/TopBar/index.js
+++ app/components/TopBar/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import Button from '../Button';
 
+import Icon from 'Components/Icon';
+
 const TopBar = (props) => {
     return (
         <div className="mp--topbar">
-            <div className="logo-container">
-                <img src="../../../images/logo.png" alt="Logo"/>
-            </div>
-            <div className="topbar-elements-container">
-                <NavLink to={'/about-us/'} activeClassName={'active'}>About Us</NavLink>
-                <NavLink to={'/work/'} activeClassName={'active'}>Work</NavLink>
-                <NavLink to={'/services/'} activeClassName={'active'}>Services</NavLink>
-                <NavLink to={'/contact/'} activeClassName={'active'}>Contact</NavLink>
-            </div>
-            <div className="get-quote-wrapper">
-                <Button palette={'secondary'}>Get Quotation</Button>
+            <div className="mp--topbar--inner">
+                <div className="logo-container">
+                    <img src="../../../images/logo.png" alt="Logo"/>
+                </div>
+                <div className="topbar-elements-container">
+                    <NavLink to={'/about-us/'} activeClassName={'active'}>About Us</NavLink>
+                    <NavLink to={'/work/'} activeClassName={'active'}>Work</NavLink>
+                    <NavLink to={'/services/'} activeClassName={'active'}>Services</NavLink>
+                    <NavLink to={'/contact/'} activeClassName={'active'}>Contact</NavLink>
+                    <NavLink to={'/contact/'} activeClassName={'active'}><Icon src={'../../../images/svg/burger-menu-icon.svg'}/></NavLink>
+                </div>
+                <div className="get-quote-wrapper">
+                    <Button palette={'secondary'}>Get Quotation</Button>
+                </div>
             </div>
         </div>
     )
